be48a8bdd52b4d5697934ceddfc445b0b631a5c0
rosettascribe <bazin.remi@gmail.com>
2014-06-13 23:09:30 +0200

    Command-line parsing

--- src/AutoAppro.java
+++ src/AutoAppro.java
@@ -8,6 +8,7 @@ import util.*;
 /** The main class for the program. */
 public class AutoAppro
 {
+	private static final String USAGE = "Usage: java -jar AutoAppro.jar [-locale lang country] [-help]";
 	/* Example values for the splash screen */
 	private static final Rectangle SPLASH_PRGSS_AREA = new Rectangle(20, 350, 600, 5);
 	private static final Color SPLASH_PRGSS_COLOR = Color.WHITE;
@@ -28,7 +29,29 @@ public class AutoAppro
 		splash = new MySplash(SPLASH_PRGSS_AREA, SPLASH_PRGSS_COLOR,
 				SPLASH_STATUS_POS, SPLASH_STATUS_COLOR);
 		/* Parsing the command-line arguments */
-		// TODO
+		try {
+			for (int i = 0; i < args.length; ++i)
+			{
+				switch (args[i])
+				{
+				case "-locale":
+					if (i + 2 >= args.length)
+						throw new IllegalArgumentException("Missing argument for the locale.");
+					Locale newLocale = new Locale(args[++i], args[++i]);
+					MyPreferences.set("locale", newLocale);
+					MyPreferences.save();
+					break;
+				case "-help":
+					System.out.println(USAGE);
+					return;
+				default:
+					throw new IllegalArgumentException("Unknown option " + args[i]);
+				}
+			}
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+			System.err.println(USAGE);
+		}
 		/* Loading the language bundle */
 		{
 			Locale locale;

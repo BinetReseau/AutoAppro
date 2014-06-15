1814106b918f165385c68219739f0e1898c4aad7
rosettascribe <bazin.remi@gmail.com>
2014-06-15 18:56:44 +0200

    Logger interface

---
+++ src/models/LogItem.java
@@ -0,0 +1,11 @@
+package models;
+
+/** An item as it should be logged at the end. */
+public class LogItem {
+	/** The ID of the product. */
+	public int barID;
+	/** The quantity for this product. */
+	public double quantity;
+	/** The price in cents for all this quantity. */
+	public double price;
+}
---
+++ src/models/Logger.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.util.List;
+
+/** The interface every logger must implement. */
+public interface Logger {
+	/** Log all the given items.
+	 *
+	 * @param items The list of items to log.
+	 * @return <code>null</code> if the function is automatic,
+	 *   or the result string to put in the clip-board.
+	 * @throws Exception If an error occurs.
+	 */
+	public String log(List<LogItem> items) throws Exception;
+}

db996b001af2085f64e1d8a6b7b6cfa3352115cd
rosettascribe <bazin.remi@gmail.com>
2014-07-01 18:59:42 +0200

    Automatic logger started

---
+++ src/loggers/Bar2Auto.java
@@ -0,0 +1,21 @@
+package loggers;
+
+import models.LogItem;
+import models.Logger;
+
+public class Bar2Auto extends Logger
+{
+	@Override
+	public String log(Iterable<LogItem> items) throws Exception
+	{
+		// TODO=
+		return null;
+	}
+
+	@Override
+	public String getName()
+	{
+		// TODO
+		return null;
+	}
+}

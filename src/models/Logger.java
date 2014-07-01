74f5a044409dd9eb3fe19ceb47f26e9d0ac64a40
rosettascribe <bazin.remi@gmail.com>
2014-07-01 18:14:17 +0200

    Logger UI possibilities improved

--- src/models/Logger.java
+++ src/models/Logger.java
@@ -1,5 +1,13 @@
 package models;
 
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+import AutoAppro.AutoAppro;
+
 /** The abstract class every logger must implement. */
 public abstract class Logger
 {
@@ -17,6 +25,67 @@ public abstract class Logger
 	 * @return The logger name.
 	 */
 	public abstract String getName();
+
+	/** Initialize the logger.
+	 * <p>
+	 * Actually, some initializing part might be in the constructor,
+	 * but the constructor may be called even if this logger is not used.
+	 * Therefore, this function is called once the logger chosen,
+	 * and only for this one.
+	 */
+	public void initialize()
+	{
+	}
+
+	/** Get the logger panel.
+	 * <p>
+	 * This function is used when a new product is found on the
+	 * provider side, to let the user choose (or not) the
+	 * correct corresponding bar item.
+	 * <p>
+	 * Note that this function is to be called from the GUI thread.
+	 *
+	 * @param isNew Check whether this is a new product.
+	 * @param defaultID The default ID (the old one if the product is not new,
+	 *   The last free one after an existing ID else).
+	 * @param providerName The name of the product as the provider sees it.
+	 * @return A new {@link LoggerPanel} object, or <code>null</code>
+	 *   if there is no need to ask the user (see {@link #getID()}).
+	 */
+	public LoggerPanel getLoggerPanel(boolean isNew, int defaultID, String providerName)
+	{
+		final JSpinner productBarID = new JSpinner(new SpinnerNumberModel(defaultID, 1, 999999, 1));
+		@SuppressWarnings("serial")
+		LoggerPanel defaultPanel = new LoggerPanel() {
+			@Override
+			public int getBarID()
+			{
+				return (int) productBarID.getValue();
+			}
+			@Override
+			public void setBarID(int id)
+			{
+				productBarID.setValue(id);
+			}
+		};
+		defaultPanel.setLayout(new GridLayout(0, 2, 0, 5));
+		defaultPanel.add(new JLabel(AutoAppro.messages.getString("product_type")));
+		defaultPanel.add(productBarID);
+		return defaultPanel;
+	}
+
+	/** The ID for the last product asked.
+	 * <p>
+	 * In case {@link #getLoggerPanel(int)} returns <code>null</code>,
+	 * this function is then used to retrieve the bar ID for the corresponding
+	 * product.
+	 *
+	 * @return The bar ID for the product.
+	 */
+	public int getID()
+	{
+		return 0;
+	}
 
 	@Override
 	public String toString()
---
+++ src/models/LoggerPanel.java
@@ -0,0 +1,19 @@
+package models;
+
+import javax.swing.JPanel;
+
+/** The panel to display when choosing the bar product. */
+@SuppressWarnings("serial")
+public abstract class LoggerPanel extends JPanel {
+	/** When the user validates the form, this should return the correct value.
+	 *
+	 * @return The bar ID for the product.
+	 */
+	public abstract int getBarID();
+
+	/** When the user wants to give it a certain ID, perhaps indirectly.
+	 *
+	 * @param id The bar ID for this product.
+	 */
+	public abstract void setBarID(int id);
+}
